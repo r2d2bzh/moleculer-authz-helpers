4684fcf2209c78169b18308779781387c1acdf41
gautaz <11046232+gautaz@users.noreply.github.com>
2021-12-15 09:54:13 +0100

    warn feature for is-authorized

--- src/is-authorized.js
+++ src/is-authorized.js
@@ -1,19 +1,62 @@
 const removeUnanswered = (authzAnswers) => authzAnswers.filter((answer) => answer !== undefined);
 
-const doAnswersAuthorize = (authzAnswers) =>
-  authzAnswers.length
-    ? authzAnswers.reduce((isAuthorized, authzAnswer) => isAuthorized && doesAnswerAuthorize(authzAnswer), true)
-    : false;
+const reduceAnswers = (answers, reducer) =>
+  answers.length
+    ? answers.reduce(
+        ([authorizationStatus, authorizationsCount], answer) =>
+          reducer(answer, authorizationStatus, authorizationsCount),
+        [true, 0]
+      )
+    : [false, 0];
 
-const doesAnswerAuthorize = function (something) {
-  switch (Object.prototype.toString.call(something)) {
-    case '[object Boolean]':
-      return something;
-    case '[object Array]':
-      return doAnswersAuthorize(removeUnanswered(something));
-    default:
-      return false;
-  }
+const onMultipleAuthorizations =
+  (handle) =>
+  ([answersAuthorization, authorizationsCount]) => {
+    if (authorizationsCount > 1) {
+      handle(authorizationsCount);
+    }
+    return answersAuthorization;
+  };
+
+const processAndCountManyAnswers = (warn) => {
+  const warnOnMultipleAuthorizations = onMultipleAuthorizations((authorizationsCount) =>
+    warn(`multiple answers to the same authorization request (${authorizationsCount})`)
+  );
+  return (answers, authorizationStatus) => {
+    const newRequestIsAuthorized = warnOnMultipleAuthorizations(
+      reduceAnswers(removeUnanswered(answers), processAndCountAnswer(warn))
+    );
+    return authorizationStatus && newRequestIsAuthorized;
+  };
 };
 
-export default doesAnswerAuthorize;
+const processAndCountAnswer = (warn) => {
+  const doAnswersAuthorize = processAndCountManyAnswers(warn);
+  return function (answer, authorizationStatus = true, authorizationsCount = 0) {
+    switch (Object.prototype.toString.call(answer)) {
+      case '[object Boolean]':
+        return [authorizationStatus && answer, authorizationsCount + 1];
+      case '[object Array]':
+        // A new array means a new authorization request with its own
+        // answers count that does not reflect on the current one
+        return [doAnswersAuthorize(answer, authorizationStatus), authorizationsCount];
+      case '[object Undefined]':
+        return [false, authorizationsCount];
+      default:
+        warn(`odd authorization answer (${answer})`);
+        return [false, authorizationsCount];
+    }
+  };
+};
+
+export default (logger) => {
+  try {
+    const doesAnswerAuthorizeAndWarn = processAndCountAnswer(logger.warn.bind(logger));
+    return function (answer) {
+      const [authorize] = doesAnswerAuthorizeAndWarn(answer);
+      return authorize;
+    };
+  } catch (e) {
+    throw new Error(`logger.warn must be a function`);
+  }
+};
--- test/is-authorized.js
+++ test/is-authorized.js
@@ -3,24 +3,54 @@ import { inspect } from 'util';
 import { isAuthorized } from '../index.js';
 
 const getTitle = (testCase) => `should${testCase.expectation ? ' ' : ' not '}authorize: ${inspect(testCase.mock)}`;
+const warn = (t) => ({
+  warn: (message) => t.snapshot(message),
+});
 const testSuite = [
   { mock: undefined, expectation: false },
-  { mock: null, expectation: false },
+  { mock: null, expectation: false, warn },
   { mock: false, expectation: false },
   { mock: [], expectation: false },
   { mock: [undefined], expectation: false },
   { mock: [false, undefined], expectation: false },
-  { mock: [false, undefined, true], expectation: false },
-  { mock: [false, true], expectation: false },
+  { mock: [false, undefined, true], expectation: false, warn },
+  { mock: [false, true], expectation: false, warn },
   { mock: [true], expectation: true },
-  { mock: [true, true], expectation: true },
+  { mock: [true, true, true], expectation: true, warn },
   { mock: [true, undefined], expectation: true },
+  { mock: [true, [true]], expectation: true },
+  { mock: [false, [true]], expectation: false },
+  { mock: [true, [false, true, true]], expectation: false, warn },
+  { mock: [false, [false, true, true]], expectation: false, warn },
   // Test answer for two concurrent authorization requests
   { mock: [[undefined], [true]], expectation: false },
+  { mock: [[null], [true]], expectation: false, warn },
+  { mock: [[true, true], [true]], expectation: true, warn },
+  {
+    mock: [
+      [true, undefined, undefined],
+      [undefined, true, undefined],
+      [undefined, undefined, false],
+    ],
+    expectation: false,
+  },
 ];
 
 testSuite.forEach((testCase) =>
   test(getTitle(testCase), (t) => {
-    t.assert(isAuthorized(testCase.mock) === testCase.expectation);
+    if (testCase.warn) {
+      t.plan(2);
+    }
+    t.assert(
+      isAuthorized(testCase.warn ? testCase.warn(t) : { warn: () => t.fail() })(testCase.mock) === testCase.expectation
+    );
   })
 );
+
+test('throws on missing logger', (t) => {
+  t.snapshot(t.throws(() => isAuthorized()));
+});
+
+test('throws on bad logger', (t) => {
+  t.snapshot(t.throws(() => isAuthorized({ warn: true })));
+});
